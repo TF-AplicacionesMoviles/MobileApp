01a1b4598a8faf815eb05a84da483c967e700953
Jesus <jesus.cont5648@gmail.com>
2025-05-14 17:02:31 -0500

    feat: added data files

--- app/src/main/java/com/example/dentifymobile/iam/data/remote/services/AuthApiService.kt
+++ app/src/main/java/com/example/dentifymobile/iam/data/remote/services/AuthApiService.kt
@@ -1,11 +1,16 @@
 package com.example.dentifymobile.iam.data.remote.services
 
 import com.example.dentifymobile.iam.data.model.LoginResponse
+import com.example.dentifymobile.iam.data.model.RegisterResponse
 import com.example.dentifymobile.iam.data.remote.dto.LoginRequest
+import com.example.dentifymobile.iam.data.remote.dto.RegisterRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApiService {
     @POST("auth/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+
+    @POST("auth/register")
+    suspend fun register(@Body registerRequest: RegisterRequest): RegisterResponse
 }

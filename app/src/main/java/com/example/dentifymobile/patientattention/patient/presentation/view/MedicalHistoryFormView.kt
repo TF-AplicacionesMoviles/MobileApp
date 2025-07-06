98637011afbd4b6beb0723b0db1f3dfae3ea2213
Lucero H <120441361+lucerohh@users.noreply.github.com>
2025-07-05 23:42:51 -0500

    feat(patientattention): Updated view styles for patients and medical histories

--- app/src/main/java/com/example/dentifymobile/patientattention/patient/presentation/view/MedicalHistoryFormView.kt
+++ app/src/main/java/com/example/dentifymobile/patientattention/patient/presentation/view/MedicalHistoryFormView.kt
@@ -21,9 +21,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
